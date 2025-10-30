7b0a83cf39c3ba07f5c687b532be243d3217ae21
Morten Munk <mortenmunk97@gmail.com>
2025-10-30 23:49:39 +0100

    blinky

--- ex4/main.c
+++ ex4/main.c
@@ -1,6 +1,25 @@
+#include "stm32f103xb.h"
 #include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_gpio.h"
+#include "stm32f1xx_hal_rcc.h"
 
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
+void init() {
+  HAL_Init();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-int main() { HAL_Init(); }
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+int main() {
+  init();
+  while (1) {
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    HAL_Delay(200);
+  }
+}
